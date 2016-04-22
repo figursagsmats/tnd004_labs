c944814c0dde6e49e59b4a72b1fcd646edc2d7b7
Gustav Lagnström <gustavlagnstrom@Gustavs-MacBook-Pro.local>
2016-04-22 08:28:23 +0200

    lab 1 klar

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -45,166 +45,167 @@ int main()
     cout << "S3 = " << S3 << endl;
     cout << "S4 = " << S4 << endl;
 
-    // /*****************************************************
-    // * TEST PHASE 2                                       *
-    // * Copy constructor                                   *
-    // ******************************************************/
-    // cout << "\nTEST PHASE 2: copy constructor\n\n";
+    /*****************************************************
+    * TEST PHASE 2                                       *
+    * Copy constructor                                   *
+    ******************************************************/
+    cout << "\nTEST PHASE 2: copy constructor\n\n";
+    
+    //Task 2.1
+    Set<int> S5(S3);
 
-    // Set<int> S5(S3);
+    cout << "S5 = " << S5 << endl;
 
-    // cout << "S5 = " << S5 << endl;
+    /*****************************************************
+    * TEST PHASE 3                                       *
+    * Assignment operator: operator=                     *
+    ******************************************************/
+    cout << "\nTEST PHASE 3: operator=\n\n";
 
-    // /*****************************************************
-    // * TEST PHASE 3                                       *
-    // * Assignment operator: operator=                     *
-    // ******************************************************/
-    // cout << "\nTEST PHASE 3: operator=\n\n";
+    S1 = S2 = S5;
 
-    // S1 = S2 = S5;
-
-    // cout << "S1 = " << S1 << endl;
-    // cout << "S2 = " << S2 << endl;
+    cout << "S1 = " << S1 << endl;
+    cout << "S2 = " << S2 << endl;
 
     // /*****************************************************
     // * TEST PHASE 4                                       *
     // * Move constructor                                   *
     // ******************************************************/
-    // cout << "\nTEST PHASE 4: move constructor\n\n";
+    cout << "\nTEST PHASE 4: move constructor\n\n";
 
-    // Set<int> *ptr_S = new Set<int>(S4);
+    Set<int> *ptr_S = new Set<int>(S4);
 
-    // Set<int> S6(std::move(*ptr_S));
+    Set<int> S6(std::move(*ptr_S));
 
-    // cout << "S6 = " << S6 << endl;
+    cout << "S6 = " << S6 << endl;
 
-    // delete ptr_S;
-
-
-    // /*****************************************************
-    // * TEST PHASE 5                                       *
-    // * move assignment operator                           *
-    // ******************************************************/
-    // cout << "\nTEST PHASE 5: move assignment operator\n\n";
-
-    // ptr_S = new Set<int>(S2);
-
-    // S6 = std::move(*ptr_S);
-
-    // cout << "S6 = " << S6 << endl;
-
-    // delete ptr_S;
+    delete ptr_S;
 
 
-    // /*****************************************************
-    // * TEST PHASE 6                                       *
-    // * is_member                                          *
-    // ******************************************************/
-    // cout << "\nTEST PHASE 6: is_member\n\n";
+    /*****************************************************
+    * TEST PHASE 5                                       *
+    * move assignment operator                           *
+    ******************************************************/
+    cout << "\nTEST PHASE 5: move assignment operator\n\n";
 
-    // for (int i = 1; i <= 5; i++)
-    // {
-    //     cout << "S1.is_member(" << i << ") ? "
-    //          << (S1.is_member(i) ? 'T' : 'F') << endl;
-    // }
-    // cout << "\nS1.is_member(99999) ? "
-    //      << (S1.is_member(99999) ? 'T' : 'F') << endl;
+    ptr_S = new Set<int>(S2);
 
-    // /*****************************************************
-    // * TEST PHASE 7                                       *
-    // * cardinality, make_clear                            *
-    // ******************************************************/
-    // cout << "\nTEST PHASE 7: cardinality\n\n";
+    S6 = std::move(*ptr_S);
 
-    // cout << "S1.cardinality() = "
-    //      << S1.cardinality() << endl;
+    cout << "S6 = " << S6 << endl;
 
-    // cout << "S1 becomes the empty set ..." << endl;
-
-    // S1.make_empty();
-
-    // cout << "S1.cardinality() = "
-    //      << S1.cardinality() << endl;
-
-    // /*****************************************************
-    // * TEST PHASE 8                                       *
-    // * Overloaded operators: operator+=, operator*=       *
-    // *                   and operator-=                   *
-    // ******************************************************/
-    // cout << "\nTEST PHASE 8: operator+=, operator*=, operator-=\n\n";
-
-    // S6 += S4;
-    // S4 *= S4;
-    // S1 -= S1;
-
-    // cout << "S6 = " << S6 << endl;
-    // cout << "S4 = " << S4 << endl;
-    // cout << "S1 = " << S1 << endl;
+    delete ptr_S;
 
 
-    // /*****************************************************
-    // * TEST PHASE 9                                       *
-    // * Overloaded operators: union, intersection, and     *
-    // * and difference                                     *
-    // ******************************************************/
-    // cout << "\nTEST PHASE 9: union, intersection, and difference\n\n";
+    /*****************************************************
+    * TEST PHASE 6                                       *
+    * is_member                                          *
+    ******************************************************/
+    cout << "\nTEST PHASE 6: is_member\n\n";
 
-    // S1 = (S3 + S4);
-    // S2 = S3 * S4;
-    // S5 = S3 - S4;
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << "S1.is_member(" << i << ") ? "
+             << (S1.is_member(i) ? 'T' : 'F') << endl;
+    }
+    cout << "\nS1.is_member(99999) ? "
+         << (S1.is_member(99999) ? 'T' : 'F') << endl;
 
-    // Set<int> S7(S3);
-    // S6 = std::move(S7) + S5;
+    /*****************************************************
+    * TEST PHASE 7                                       *
+    * cardinality, make_clear                            *
+    ******************************************************/
+    cout << "\nTEST PHASE 7: cardinality\n\n";
 
-    // cout << "S1 = " << S1 << endl;
-    // cout << "S2 = " << S2 << endl;
-    // cout << "S3 = " << S3 << endl;
-    // cout << "S4 = " << S4 << endl;
-    // cout << "S5 = " << S5 << endl;
-    // cout << "S6 = " << S6 << endl;
+    cout << "S1.cardinality() = "
+         << S1.cardinality() << endl;
 
-    // /*****************************************************
-    // * TEST PHASE 10                                       *
-    // * Overloaded operators: equality, subset, and        *
-    // * strict subset                                      *
-    // ******************************************************/
-    // cout << "\nTEST PHASE 10: equality, subset, strict subset\n\n";
+    cout << "S1 becomes the empty set ..." << endl;
 
-    // cout << "S4 == S1 ? " << (S4 == S1 ? 'T' : 'F') << endl;
-    // cout << "S4!== S1 ? " << (S4 != S1 ? 'T' : 'F') << endl;
-    // cout << "S4 <= S1 ? " << (S4 <= S1 ? 'T' : 'F') << endl;
-    // cout << "S4 <  S1 ? " << (S4 <  S1 ? 'T' : 'F') << endl;
+    S1.make_empty();
 
-    // /*****************************************************
-    // * TEST PHASE 11                                       *
-    // * Overloaded operators: mixed-mode arithmetic        *
-    // ******************************************************/
-    // cout << "\nTEST PHASE 11: mixed-mode arithmetic\n\n";
+    cout << "S1.cardinality() = "
+         << S1.cardinality() << endl;
 
-    // //Note: conversion constructor is called
-    // S2 = 4 + S3 - 5 - (S3+S4) - 99999;
-    // S5 = S4 - 2 + S2 + 24;
+    /*****************************************************
+    * TEST PHASE 8                                       *
+    * Overloaded operators: operator+=, operator*=       *
+    *                   and operator-=                   *
+    ******************************************************/
+    cout << "\nTEST PHASE 8: operator+=, operator*=, operator-=\n\n";
 
-    // cout << "S2 = " << S2 << endl;
-    // cout << "S5 = " << S5 << endl;
+    S6 += S4;
+    S4 *= S4;
+    S1 -= S1;
 
-    // /*****************************************************
-    // * TEST PHASE 12                                       *
-    // * Create a set of strings                            *
-    // ******************************************************/
-    // cout << "\nTEST PHASE 12: Create a set of strings\n\n";
+    cout << "S6 = " << S6 << endl;
+    cout << "S4 = " << S4 << endl;
+    cout << "S1 = " << S1 << endl;
 
-    // string AA[] = {"000", "001", "010", "011", "100"};
-    // Set<string> words(AA, 5);
 
-    // words = string("101") + words - string("000");
+    /*****************************************************
+    * TEST PHASE 9                                       *
+    * Overloaded operators: union, intersection, and     *
+    * and difference                                     *
+    ******************************************************/
+    cout << "\nTEST PHASE 9: union, intersection, and difference\n\n";
 
-    // cout << "The binary strings sorted: "
-    //      << words << endl;
+    S1 = (S3 + S4);
+    S2 = S3 * S4;
+    S5 = S3 - S4;
 
-    // words.make_empty();
+    Set<int> S7(S3);
+    S6 = std::move(S7) + S5;
 
-    // cout << "The binary strings: " << words << endl;
+    cout << "S1 = " << S1 << endl;
+    cout << "S2 = " << S2 << endl;
+    cout << "S3 = " << S3 << endl;
+    cout << "S4 = " << S4 << endl;
+    cout << "S5 = " << S5 << endl;
+    cout << "S6 = " << S6 << endl;
+
+    /*****************************************************
+    * TEST PHASE 10                                       *
+    * Overloaded operators: equality, subset, and        *
+    * strict subset                                      *
+    ******************************************************/
+    cout << "\nTEST PHASE 10: equality, subset, strict subset\n\n";
+
+    cout << "S4 == S1 ? " << (S4 == S1 ? 'T' : 'F') << endl;
+    cout << "S4!== S1 ? " << (S4 != S1 ? 'T' : 'F') << endl;
+    cout << "S4 <= S1 ? " << (S4 <= S1 ? 'T' : 'F') << endl;
+    cout << "S4 <  S1 ? " << (S4 <  S1 ? 'T' : 'F') << endl;
+
+    /*****************************************************
+    * TEST PHASE 11                                       *
+    * Overloaded operators: mixed-mode arithmetic        *
+    ******************************************************/
+    cout << "\nTEST PHASE 11: mixed-mode arithmetic\n\n";
+
+    //Note: conversion constructor is called
+    S2 = 4 + S3 - 5 - (S3+S4) - 99999;
+    S5 = S4 - 2 + S2 + 24;
+
+    cout << "S2 = " << S2 << endl;
+    cout << "S5 = " << S5 << endl;
+
+    /*****************************************************
+    * TEST PHASE 12                                       *
+    * Create a set of strings                            *
+    ******************************************************/
+    cout << "\nTEST PHASE 12: Create a set of strings\n\n";
+
+    string AA[] = {"000", "001", "010", "011", "100"};
+    Set<string> words(AA, 5);
+
+    words = string("101") + words - string("000");
+
+    cout << "The binary strings sorted: "
+         << words << endl;
+
+    words.make_empty();
+
+    cout << "The binary strings: " << words << endl;
 
 	cout << "Ending ...." << endl;
 
--- lab1/set.h
+++ lab1/set.h
@@ -8,19 +8,42 @@ template <typename T>
 class Set
 {
 public:
+    //CONSTRUCTORS ETC
     Set(); //default constructor
     Set(T data);
+    Set(T data[],int size); 
     Set(const Set& s); //copy constructor
-    Set(T data[],int size);
-    //operator=(const T&); //assignment
+    Set(Set&& s); //move constructor
+    
     ~Set(); //desctructor
     
+    //OPERATORS
+    Set& operator=(const Set& s); //copy asign
+    Set& operator=(Set&& s); //move assign
+    Set& operator+=(const Set& s); //union
+    Set& operator*=(const Set& s); //intersection
+    Set& operator-=(const Set& s); //difference
+    
+    bool operator<=(const Set& s); //subset
+    bool operator!=(const Set& s); //different
+    
+    bool operator<(const Set& s) 
+    {
+        return cardinality() > s.cardinality() && operator<=(s);
+    }
+    bool operator==(const Set& s) 
+    {
+        return cardinality() == s.cardinality() && operator<=(s);
+    }
+    //METHODS
     bool isEmpty() const;
-    void insertLast(T data);
+    void make_empty();
+    bool is_member(const T&) const;
+    int cardinality() const;
+    
     
 private:
-    class Node
-    {
+    class Node {
     public:
         typedef shared_ptr<Node> NodePtr;
         
@@ -38,30 +61,67 @@ private:
             id = i;
         }       
     };
-    typedef shared_ptr<Node> NodePtr;    
+    
+    typedef shared_ptr<Node> NodePtr; 
+       
     //PRIVATE VARIABLES
     NodePtr head;  
     NodePtr tail;
     int nNodes = 0;
     
     //PRIVATE METHODS
+    
+    friend Set operator+(Set lhs, const Set& rhs) {
+        return lhs+=rhs; //works nice since lhs is passed by copy!
+    }
+    
+    friend Set operator*(Set lhs, const Set& rhs) {
+        return lhs*=rhs; //works nice since lhs is passed by copy!
+    }
+    
+    friend Set operator-(Set lhs, const Set& rhs) {
+        return lhs-=rhs; //works nice since lhs is passed by copy!
+    }
+    
     friend ostream& operator<<(ostream& os, const Set(& s)){
+        cout << "{ ";
         if(s.isEmpty()){
-           os << "EMPTY"; 
+           os << "EMPTY "; 
         } else{
             for(NodePtr np = s.head->next; np != s.tail; np=np->next){
                 os << np->data << " ";   
-            }
-                   
-        } 
+            }                   
+        }
+        cout << "}" ;
         return os;
     }
+    
+    void insertLast(T data) {
+        nNodes++;
+        tail->prev->next = make_shared<Node>(data,tail,tail->prev,nNodes);
+        tail->prev = tail->prev->next;
+    }
+    void insertAt(NodePtr pos, T data) {
+        nNodes++;
+        NodePtr tmp = pos->prev;
+        pos->prev = make_shared<Node>(data,pos, tmp,nNodes);
+        tmp->next = pos->prev;
+    }
+    void deleteAt(NodePtr node) {
+        nNodes--;
+        NodePtr tmp = node;
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+        tmp = nullptr;
+    }
+    
 };
 
+//CONSTRUCTORS ETC
 
+// default constructor
 template <typename T>
-Set<T>::Set()
-{
+Set<T>::Set(){
     //cout << "Set default constructor" << endl;
     
     //Create an empty set. This means head points to tail an vice versa
@@ -76,18 +136,16 @@ Set<T>::Set()
     
 }
 
-
-
-
+//assignment
 template <typename T>
-Set<T>::Set(T data) : Set()
-{
+Set<T>::Set(T data) : Set(){
     
     head->next = make_shared<Node>(data,tail,head);
     tail->prev = head->next;
     
 }
 
+//assignment
 template <typename T>
 Set<T>::Set(T data[],int size) : Set() {
     //loop array assuming that user input correct size (retarded)
@@ -98,31 +156,224 @@ Set<T>::Set(T data[],int size) : Set() {
     }
 }
 
+//copy constructor
 template <typename T>
 Set<T>::Set(const Set& s) : Set()
 {
+    //Task 2.1
+    
+    if(s.isEmpty()){
+    } else {
+        for(NodePtr node = s.head->next; node != s.tail; node = node->next ){
+            insertLast(node->data);
+        }   
+    }
+}
+
+//move constructor
+template <typename T>
+Set<T>::Set( Set&& s) : Set()
+{
+    cout << "Move constructor called..." << endl;
+    head = s.head; //nodes are "stolen" form s
+    tail = s.tail;
+    s.head = s.tail = nullptr; //dosnt leave an empty set
+}
+
+//desctructor
+template <typename T>
+Set<T>::~Set(){
+    // cout << "destructor for SET (MOVE dosnt leave any nodes(head/tail)\n";
+    NodePtr next = head;
+    NodePtr tmp;
+    
+    while(next != tail){
+        tmp = next;
+        next = next->next;
+        tmp->next = nullptr; 
+    }
+    head = tail = nullptr;
+    //By not pointing to the nodes (shared_ptr) they should invoke 'delete' naturally
+}
+
+//OPERATORS
+
+//assignment operator
+template <typename T> 
+Set<T>& Set<T>::operator=( const Set<T> & s ){ 
+    //Task 2.2
+      
+    if(!isEmpty()) {
+        make_empty();
+    }
+       
+    if(s.isEmpty()) {
+        return *this;
+    }
+     
+    for(NodePtr node = s.head->next; node != s.tail; node = node->next){
+        insertLast(node->data);
+    }
+    return *this;
+    
+    
+}
+
+//move operator
+template <typename T> 
+Set<T>& Set<T>::operator=( Set && s ){
+    if(!isEmpty()) {
+        make_empty();
+    }
+    
+    head = s.head; //nodes are "stolen" form s
+    tail = s.tail;
+    s.head = s.tail = nullptr; //dosnt leave an empty set
+    
+    return *this;
+   
 }
 
 template <typename T>
-Set<T>::~Set()
+Set<T>& Set<T>::operator+=(const Set& s )
 {
+    // cout << "Union operator\n";
+    NodePtr tmp;
+    NodePtr c1 = head->next;//c1 keeps track of the original set 
+    for(NodePtr c2 = s.head->next; c2 != s.tail; c2 = c2->next) //c2 itterates 's'
+    {
+        if(!is_member(c2->data))
+        {
+            //if larger than last value. insert at back
+            if(c2->data > tail->prev->data){
+                c1 = tail;
+            } else {
+                
+            //else find position within set
+            while(c1->data < c2->data )
+                c1 = c1->next;
+            }
+            
+            //insert at updated c1 position
+            insertAt(c1,c2->data);
+        }    
+    }
+    return *this;
 }
 
 
 template <typename T>
-bool Set<T>::isEmpty() const
+Set<T>& Set<T>::operator-=(const Set& s )
 {
+    NodePtr tmp;
+    for(NodePtr node = head->next; node != tail; node = node->next){
+        
+        if(s.is_member(node->data)) { //remove
+            
+            deleteAt(node);
+        }
+    }
+    
+    return *this;
+}
+
+template <typename T>
+Set<T>& Set<T>::operator*=(const Set& s )
+{
+    // cout << "differance operator\n";
+    NodePtr tmp;
+    for(NodePtr node = head->next; node != tail; node = node->next){
+        
+        if(!s.is_member(node->data)) { //remove
+            
+            deleteAt(node);
+        }
+    }
+    
+    return *this;
+}
+
+template <typename T>
+bool Set<T>::operator<=(const Set& s )
+{
+    // Set s is a subset of R(this) if and only if every member
+    // of s is a member of R(this)
+    for(NodePtr node = head->next; node != tail; node = node->next)
+        if(!s.is_member(node->data)){
+            return false;
+        }
+        return true;    
+}
+
+template <typename T>
+bool Set<T>::operator!=(const Set& s )
+{
+    //differen operator. 
+    if(cardinality() != s.cardinality()){
+        return true;
+    }
+    for(NodePtr node = head->next; node != tail; node = node->next){
+        if(!s.is_member(node->data)){
+            return true;
+        }  
+    }   
+    return false;
+}
+
+//METHODS
+template <typename T>
+bool Set<T>::isEmpty() const {
+    
+    //This can only happen in copy constructor or assignment operator
+    if(head == nullptr && tail == nullptr) {
+        return true;
+    }
     if(tail->prev == head || head->next == tail){
         return true;
     }
     return false;
 }
+
+
 template <typename T>
-void Set<T>::insertLast(T data)
+void Set<T>::make_empty(){
+    if(isEmpty()){
+        return; 
+    }
+        
+    // Removing all connections in one direction and removing pointers to
+    // first and object will make smart pointer deletetion propagate through
+    // the list.
+    NodePtr nextNode = head->next; 
+    NodePtr tmp;
+    nNodes = 0;
+    while(nextNode->next != tail){
+        tmp = nextNode;
+        nextNode = nextNode->next;
+        tmp->next = nullptr; 
+    }
+    head->next = tail; 
+    tail->prev = head;
+}
+
+template <typename T>
+bool Set<T>::is_member(const T& v) const
 {
-    
-    //cout << "hep" << tail->prev->next << endl;
-    nNodes++;
-    tail->prev->next = make_shared<Node>(data,tail,tail->prev,nNodes);
-    tail->prev = tail->prev->next;
+    for (NodePtr node = head->next; node != tail; node = node->next) 
+        if(node->data == v) {
+            return true;
+        }
+    return false;
+}
+
+template <typename T>
+int Set<T>::cardinality() const
+{
+    // if(isEmpty())
+    //     return 0;
+    // int count = 0;
+    // for (NodePtr node = head; node->next != tail; node = node->next, count++) { 
+        
+    // }
+    return nNodes;
 }
